9fd37e0ea43d8dbec873af0c0a3841e6fcad104a
David Mrabu <davidjblaze@hotmail.com>
2017-10-31 11:45:13 +0300

    all content is properly indented

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("#nav").hover(
-    function(){
-    $("hr").css({"transition-duration":" 2s","transform":"rotate(360deg)"});
-    $(".links").css("display","block");
-     },
-     function(){
-     $("hr").css({"transition-duration":" 0s","transform":"rotate(0deg)"});
-     $(".links").css("display","none");
-     }
+    function() {
+      $("hr").css({
+        "transition-duration": " 2s",
+        "transform": "rotate(360deg)"
+      });
+      $(".links").css("display", "block");
+    },
+    function() {
+      $("hr").css({
+        "transition-duration": " 0s",
+        "transform": "rotate(0deg)"
+      });
+      $(".links").css("display", "none");
+    }
 
 
-);
+  );
 
-    // $("hr").mouseenter({"transition-duration":" 2s","transform":"rotate(360deg)"}).
-    // mouseleave({"transition-duration":"0","transform":"rotate(0deg)"});
+  // $("hr").mouseenter({"transition-duration":" 2s","transform":"rotate(360deg)"}).
+  // mouseleave({"transition-duration":"0","transform":"rotate(0deg)"});
 
 });
